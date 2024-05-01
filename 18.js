8b6110340a2ab428969ca8cd9e1b28b906ba9aa5
JaehunCheon <dhsslwogns@naver.com>
2024-05-01 09:35:37 +0900

    문자열을 정수로 바꾸기

---
+++ 18.js
@@ -0,0 +1,13 @@
+// 문자열을 정수로 바꾸기
+function solution(s) {
+    var answer = 0;
+    answer = Number(s);
+
+    return answer;
+}
+
+let s = "-1234";
+
+
+console.log(solution(s));
+console.log(typeof solution(s));

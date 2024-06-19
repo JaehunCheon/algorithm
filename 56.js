6b1dd225e30e0cb0835a2eea5efe7b9c068a18fd
JaehunCheon <dhsslwogns@naver.com>
2024-06-20 01:06:43 +0900

    과일장수

---
+++ 56.js
@@ -0,0 +1,10 @@
+// 과일 장수
+
+function solution(k, m, score) {
+    let answer = 0;
+    const sortedScore = score.sort((a, b) => a - b).slice(score.length % m);
+    for (let i = 0; i < sortedScore.length; i += m) {
+        answer += sortedScore[i] * m;
+    }
+    return answer;
+}

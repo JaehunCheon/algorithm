793dce7a9b0edb8f13d25fc2af15a01498106672
JaehunCheon <dhsslwogns@naver.com>
2024-06-20 09:34:12 +0900

    소수 만들기

---
+++ 58.js
@@ -0,0 +1,23 @@
+// 소수 만들기
+
+function primecheck(n){
+    for(var i=2;i<=Math.sqrt(n);i++){
+        if(n%i == 0){
+            return false;
+        }
+    }
+    return true;    
+}
+function solution(nums){
+    var cnt = 0;
+    for(var i=0;i<nums.length-2;i++){
+        for(var j=i+1;j<nums.length-1;j++){
+            for(var w=j+1;w<nums.length;w++){
+                if(primecheck(nums[i]+nums[j]+nums[w])){
+                    cnt++;
+                }
+            }
+        }
+    }
+    return cnt;
+}
